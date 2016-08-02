ae21f62dd6559488960511aa33f51fd681b57ce1
bjuppa <nilsved@gmail.com>
2016-08-02 17:20:15 +0200

    implementation of setRequestInput()

--- src/Support/ModifiesRequestInputTrait.php
+++ src/Support/ModifiesRequestInputTrait.php
@@ -1,6 +1,9 @@
 <?php
 namespace FewAgency\Reformulator\Support;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
 trait ModifiesRequestInputTrait
 {
     /* Replaces a single item in a given Request’s input using dot-notation (to be used by Middleware or FormRequest) */
@@ -14,6 +17,13 @@ trait ModifiesRequestInputTrait
         …but they both can only find a full string key OR dot notated key
         Request::merge() adds/overwrites an array of values to the input
         */
-        //TODO: implement setRequestInput()
+        if (strpos($key, '.')) {
+            list($key, $key_rest) = explode('.', $key, 2);
+            $new_value = $request->input($key);
+            Arr::set($new_value, $key_rest, $value);
+        } else {
+            $new_value = $value;
+        }
+        $request->merge([$key => $new_value]);
     }
 }
