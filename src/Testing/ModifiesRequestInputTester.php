c8a5469e01ffa2714cd18bd1e70100e15875947d
bjuppa <nilsved@gmail.com>
2016-08-02 17:18:51 +0200

    A class using the trait for testing

---
+++ src/Testing/ModifiesRequestInputTester.php
@@ -0,0 +1,17 @@
+<?php
+namespace FewAgency\Reformulator\Testing;
+
+use FewAgency\Reformulator\Support\ModifiesRequestInputTrait;
+use Illuminate\Http\Request;
+
+class ModifiesRequestInputTester
+{
+    use ModifiesRequestInputTrait;
+
+    public function testSetRequestInput(Request $request, $key, $value)
+    {
+        return $this->setRequestInput($request, $key, $value);
+    }
+
+
+}
