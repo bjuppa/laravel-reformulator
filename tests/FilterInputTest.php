0296e7766a5adcf5bf0ac439d5f52ac6ccdd00f1
bjuppa <nilsved@gmail.com>
2016-08-03 11:54:25 +0200

    testing multiple fields

--- tests/FilterInputTest.php
+++ tests/FilterInputTest.php
@@ -19,15 +19,15 @@ class FilterInputTest extends PHPUnit_Framework_TestCase
     {
         $middleware = new \FewAgency\Reformulator\Middleware\FilterInput();
         $request = new \Illuminate\Http\Request();
-        $request['a'] = ' test ';
-        $request['b'] = ' test ';
+        $request['a'] = ' test A ';
+        $request['b'] = ' test B ';
 
         $result_request = $middleware->handle($request, function ($request) {
             return $request;
         }, 'trim', 'a');
 
-        $this->assertEquals('test', $result_request->a);
-        $this->assertEquals(' test ', $result_request->b);
+        $this->assertEquals('test A', $result_request->a);
+        $this->assertEquals(' test B ', $result_request->b);
     }
 
     public function testTrimArray()
@@ -56,5 +56,20 @@ class FilterInputTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('0', $result_request->a[0]);
         $this->assertEquals(' 1 ', $result_request->a[1]);
+    }
+
+    public function testTrimMultipleFields()
+    {
+        $middleware = new \FewAgency\Reformulator\Middleware\FilterInput();
+        $request = new \Illuminate\Http\Request();
+        $request['a'] = ' test A ';
+        $request['b'] = ' test B ';
+
+        $result_request = $middleware->handle($request, function ($request) {
+            return $request;
+        }, 'trim', 'a', 'b');
+
+        $this->assertEquals('test A', $result_request->a);
+        $this->assertEquals('test B', $result_request->b);
     }
 }
