de4f423bb5a4858ca9d27244e86a56a156c43cf1
bjuppa <nilsved@gmail.com>
2016-08-04 13:32:00 +0200

    finished concatenating middleware

--- src/Middleware/ConcatenateInput.php
+++ src/Middleware/ConcatenateInput.php
@@ -4,13 +4,16 @@ namespace FewAgency\Reformulator\Middleware;
 
 use Closure;
 use FewAgency\Reformulator\Support\ModifiesRequestInputTrait;
+use Illuminate\Support\Arr;
 
 /*
  * Concatenate strings from request inputs or arrays.
- * The field to modify is the first parameter (dot-notated).
+ * The field to set is the first parameter (dot-notated).
  * The concatenating string is the second parameter (defaults to comma-sign if omitted)
- * The rest of the arguments are other fields (dot-notated) to concatenate into the first field.
+ * The rest of the arguments are the fields (dot-notated) to concatenate into the first field.
  * If no other fields are specified, the target field will be concatenated if it's an array.
+ * If any of the fields to concatenate is empty, that and the rest of the fields will be left out of concatenation.
+ * Empty values inside arrays will *not* stop the concatenation, so make sure you've done clean_array/remove_empty on any arrays.
  *
  * Example for route definition:
  * 'middleware' => 'reformulator.concatenate:tags'
@@ -31,26 +34,32 @@ class ConcatenateInput
      * @param  \Closure $next
      * @param  string $target_field
      * @param  string $glue
-     * @param  string ,... $source_field
+     * @param  string,... $source_field
      * @return mixed
      */
     public function handle($request, Closure $next, $target_field, $glue = 'COMMA', $source_field = null)
     {
+        //Commas can’t be used by \Illuminate\Pipeline\Pipeline::parsePipeString
+        $glue = str_replace('COMMA', ',', $glue);
         $source_fields = array_slice(func_get_args(), 4);
-        $source_values = [];
+
+        $concat_values = [];
         if (count($source_fields)) {
             foreach ($source_fields as $source_field) {
                 $source_value = $request->input($source_field);
-                if (is_array($source_value)) {
-                    $source_values = array_merge($source_values, $source_value);
+                if (is_array($source_value) or strlen($source_value)) {
+                    $concat_values[] = $source_value;
+                } else {
+                    //This value is empty so stop here and only concatenate the collected values
+                    break;
                 }
-                $source_values[] = $source_value;
             }
         } else {
-            $source_values = (array)$request->input($target_field);
+            $concat_values = (array)$request->input($target_field);
         }
-        $glue = str_replace('COMMA', ',', $glue);
-        $this->setRequestInput($request, $target_field, implode($glue, $source_values));
+
+        $concat_values = Arr::flatten($concat_values);
+        $this->setRequestInput($request, $target_field, implode($glue, $concat_values));
 
         return $next($request);
     }
--- tests/ConcatenateInputTest.php
+++ tests/ConcatenateInputTest.php
@@ -15,7 +15,55 @@ class ConcatenateInputTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('A,B,,C', $result_request->a);
     }
 
-    //TODO: test concatenating hour, minute, second into time with :
+    public function testConcatTime()
+    {
+        $middleware = new \FewAgency\Reformulator\Middleware\ConcatenateInput();
+        $request = new \Illuminate\Http\Request();
+        $request->merge(['hours' => 10, 'minutes' => 20, 'seconds' => 30]);
 
-    //TODO: test concat hour, minute when minute is empty string - this should not set the target field
+        $result_request = $middleware->handle($request, function ($request) {
+            return $request;
+        }, 'time', ':', 'hours', 'minutes', 'seconds');
+
+        $this->assertEquals('10:20:30', $result_request->time);
+    }
+
+    public function testConcatEmpty()
+    {
+        $middleware = new \FewAgency\Reformulator\Middleware\ConcatenateInput();
+        $request = new \Illuminate\Http\Request();
+        $request->merge(['hours' => 10, 'minutes' => 20, 'seconds' => '']);
+
+        $result_request = $middleware->handle($request, function ($request) {
+            return $request;
+        }, 'time', ':', 'hours', 'minutes', 'seconds');
+
+        $this->assertEquals('10:20', $result_request->time);
+    }
+
+    public function testConcatNotOverwriting()
+    {
+        $middleware = new \FewAgency\Reformulator\Middleware\ConcatenateInput();
+        $request = new \Illuminate\Http\Request();
+        $request->merge(['a' => ['a' => 1], 'b' => ['a' => 2]]);
+
+        $result_request = $middleware->handle($request, function ($request) {
+            return $request;
+        }, 'result','COMMA','a','b');
+
+        $this->assertEquals('1,2', $result_request->result);
+    }
+
+    public function testConcatDeepArray()
+    {
+        $middleware = new \FewAgency\Reformulator\Middleware\ConcatenateInput();
+        $request = new \Illuminate\Http\Request();
+        $request->merge(['a' => ['A', 'b' => ['B']]]);
+
+        $result_request = $middleware->handle($request, function ($request) {
+            return $request;
+        }, 'a');
+
+        $this->assertEquals('A,B', $result_request->a);
+    }
 }
